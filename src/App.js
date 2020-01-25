b0f047de350e4e4b8fd3c833ce329834f248b67a
Mauricio Duarte <prussdev@gmail.com>
2020-01-24 22:27:47 -0300

    Changed static item to real

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -12,15 +14,35 @@ class App extends Component {
   };
 
   handleUpload = files => {
-    console.log(files);
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
+
+
   };
 
   render() {
+    const { uploadedFiles } = this.state;
+
     return (
       <Container> 
         <Content>
           <Upload onUpload={this.handleUpload} />
-          <FileList />
+          { !!uploadedFiles.length && (
+            <FileList files={uploadedFiles} />
+          )}
         </Content>
         <GlobalStyle />
       </Container>
--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -4,39 +4,50 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="https://uploadexample-rocketseat.s3-sa-east-1.amazonaws.com/1eec1a6026c456f38f0d50b4a01f2ef2-Sem%20T%C3%ADtulo-1.png" />
+    {files.map(uploadedFile => (
+      <li>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize}
+              { !!uploadedFile.url && (
+                 <button onClick={() => { }}>Excluir</button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
+
         <div>
-          <strong>progile.png</strong>
-          <span>64kb <button onClick={() => { }}>Excluir</button></span>
+          {!uploadedFile.uploadedFile && !uploadedFile.error && (
+            <CircularProgressbar
+              styles={{
+                root: { width: 24 },
+                path: { stroke: "#7159c1" }
+              }}
+              strokeWidth={10}
+              value={uploadedFile.progress}
+            />
+          )}
+
+          {uploadedFile.url && (
+            <a
+              href="https://uploadexample-rocketseat.s3-sa-east-1.amazonaws.com/1eec1a6026c456f38f0d50b4a01f2ef2-Sem%20T%C3%ADtulo-1.png"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+
+          {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+          {uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
-      </FileInfo>
-
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: "#7159c1" }
-          }}
-          strokeWidth={10}
-          value={60}
-        />
-
-        <a
-          href="https://uploadexample-rocketseat.s3-sa-east-1.amazonaws.com/1eec1a6026c456f38f0d50b4a01f2ef2-Sem%20T%C3%ADtulo-1.png"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-
-         <MdCheckCircle size={24} color="#78e5d5" />
-         <MdError size={24} color="#e57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
